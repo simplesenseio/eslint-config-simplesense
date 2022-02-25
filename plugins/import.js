d7d2df73a7f2cc346c1b92259bdc75d9d7526885
Patrick Severy <psevery33@yahoo.com>
2022-02-25 08:35:39 -0700

    fix: resolve extraneous dependencies in lambda layer and root

--- plugins/import.js
+++ plugins/import.js
@@ -1,6 +1,10 @@
 (() => {
   'use strict';
 
+  // const path = require('path');
+
+  // const ROOT_DIR = path.resolve(__dirname, '../../../');
+
   module.exports = {
     'import/default': ['error'],
     'import/export': ['error'],
@@ -14,7 +18,8 @@
     'import/no-deprecated': ['error'],
     'import/no-duplicates': ['error'],
     'import/no-dynamic-require': ['error'],
-    'import/no-extraneous-dependencies': ['error'],
+    // There is an open merge that needs to happen before we can change this rule: https://github.com/import-js/eslint-plugin-import/pull/1954
+    'import/no-extraneous-dependencies': 'error', // [ 'error', { packageDir: [ ROOT_DIR, `${ ROOT_DIR }/lambda/layers/node-modules/nodejs` ]}],
     'import/no-named-as-default-member': ['error'],
     'import/no-named-default': ['error'],
     'import/no-self-import': ['error'],
