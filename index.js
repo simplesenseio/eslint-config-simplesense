b767958c00e63c0c32370ca342ba116e67ecce9d
Matt Wilcox <nanpx@users.noreply.github.com>
2023-05-10 16:13:45 -0600

    feat: adds rules for yaml file extension and consistent indentation

--- index.js
+++ index.js
@@ -80,6 +80,21 @@
         extends: ['plugin:yml/standard'],
         plugins: ['yml'],
         rules: {
+          'yml/block-sequence': [ 'error', 'always' ],
+          'yml/file-extension': [
+            'error',
+            {
+              extension: 'yaml',
+              caseSensitive: true,
+            },
+          ],
+          'yml/indent': [
+            'error',
+            2,
+            {
+              indentBlockSequences: true,
+            },
+          ],
           'yml/no-empty-document': ['off'],
           'yml/quotes': [
             'error', {
