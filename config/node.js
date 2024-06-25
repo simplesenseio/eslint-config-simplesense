033ea60e417fb4f28e88ca78bbef300e77f9feb6
Matt Wilcox <nanpx@users.noreply.github.com>
2024-06-25 16:06:28 -0600

    fix: `eslint-plugin-n` shared settings for resolving paths, node version, convert, and allow modules

--- config/node.js
+++ config/node.js
@@ -1,11 +1,24 @@
 (() => {
   'use strict';
 
+  const path = require('path');
+
+  const ROOT_DIR = path.resolve(__dirname, '../../../'); // this will be in <PROJECT>/node_modules/eslint-config-simplesense/rules/
+  const NODE_VERSION = '>=16.0.0';
+
   module.exports = {
     name: 'simplesense/node',
     plugins: {
       n: require('eslint-plugin-n'),
     },
     rules: require('../rules/node'),
+    settings: {
+      allowModules: ['aws-sdk'],
+      convertPath: {
+        [`${ path.relative(ROOT_DIR, '/opt/nodejs') }/*`]: [ '^(.*?)/opt/nodejs/(.*?)$', 'lambda/layers/$2/nodejs/$2' ],
+      },
+      resolvePaths: [path.resolve(__dirname, `${ ROOT_DIR }/lambda/layers/node-modules/nodejs/node_modules`)],
+      version: NODE_VERSION,
+    },
   };
 })();
--- rules/node.js
+++ rules/node.js
@@ -1,11 +1,6 @@
 (() => {
   'use strict';
 
-  const path = require('path');
-
-  const NODE_VERSION = '>=16.0.0';
-  const ROOT_DIR = path.resolve(__dirname, '../../../'); // this will be in <PROJECT>/node_modules/eslint-config-simplesense/rules/
-
   module.exports = {
     'n/callback-return': [
       'error', [
@@ -18,18 +13,9 @@
     // not used because it does not support IIFE
     // 'n/global-require': ['error'],
     'n/handle-callback-err': ['error'],
-    'n/no-deprecated-api': [
-      'error', {
-        version: NODE_VERSION,
-      },
-    ],
+    'n/no-deprecated-api': ['error'],
     'n/no-missing-import': ['error'],
-    'n/no-missing-require': [
-      'error', {
-        allowModules: ['aws-sdk'],
-        resolvePaths: [path.resolve(__dirname, `${ ROOT_DIR }/lambda/layers/node-modules/nodejs/node_modules`)],
-      },
-    ],
+    'n/no-missing-require': ['error'],
     'n/no-mixed-requires': [
       'error', {
         allowCall: true,
@@ -38,26 +24,12 @@
     ],
     'n/no-new-require': ['error'],
     'n/no-path-concat': ['error'],
-    'n/no-unsupported-features/es-builtins': [
-      'error', {
-        version: NODE_VERSION,
-      },
-    ],
+    'n/no-unsupported-features/es-builtins': ['error'],
     'n/no-unsupported-features/es-syntax': ['off'],
-    'n/no-unsupported-features/node-builtins': [
-      'error', {
-        version: NODE_VERSION,
-      },
-    ],
+    'n/no-unsupported-features/node-builtins': ['error'],
     'n/no-unpublished-bin': ['error'],
     'n/no-unpublished-import': ['error'],
-    'n/no-unpublished-require': [
-      'error', {
-        convertPath: {
-          [`${ path.relative(ROOT_DIR, '/opt/nodejs') }/*`]: [ '^(.*?)/opt/nodejs/(.*?)$', 'lambda/layers/$2/nodejs/$2' ],
-        },
-      },
-    ],
+    'n/no-unpublished-require': ['error'],
     'n/prefer-global/buffer': [ 'error', 'always' ],
     'n/prefer-global/console': [ 'error', 'always' ],
     'n/prefer-global/process': [ 'error', 'always' ],
