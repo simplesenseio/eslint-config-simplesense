b6ec2371824c418ed0a9054e2815dc212ffebb1a
Matt Wilcox <nanpx@users.noreply.github.com>
2024-06-26 09:23:30 -0600

    refactor: turns off rule `sonarjs/no-inverted-boolean-check` as it can cause confusing conditionals

--- rules/sonarjs.js
+++ rules/sonarjs.js
@@ -5,6 +5,7 @@
     'sonarjs/cognitive-complexity': ['off'],
     // this rule does has a lot of false positives
     'sonarjs/no-duplicate-string': ['off'],
+    'sonarjs/no-inverted-boolean-check': ['off'],
     'sonarjs/no-nested-template-literals': ['off'],
     'sonarjs/prefer-single-boolean-return': ['off'],
   };
